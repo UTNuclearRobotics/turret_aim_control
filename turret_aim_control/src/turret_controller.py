0e42c3a27add5faadaddb4824de9f76e984572a5
jonathan salfity <j.salfity@utexas.edu>
2024-05-10 11:55:21 -0500

    april tag detection cb function because tf frames look up last tf frame

--- turret_aim_control/src/turret_controller.py
+++ turret_aim_control/src/turret_controller.py
@@ -4,26 +4,45 @@ import rospy
 import tf
 
 from interbotix_xs_msgs.msg import JointGroupCommand
+from interbotix_xs_msgs.srv import RobotInfo
+from apriltag_ros.msg import AprilTagDetectionArray
 
 from turret_aim_control_interfaces.srv import AimEnable, AimEnableResponse
 
 TURRENT_INFO_SERVICE_NAME = "/pxxls/get_robot_info"
 PTU_COMMAND_TOPIC = "/pxxls/commands/joint_group"
 BASE_FRAME_ID = "/pxxls/base_link"
-TILT_TF_FRAME_ID = "pxxls/tilt_link"
+TILT_TF_FRAME_ID = "/pxxls/tilt_link"
 MAP_FRAME_ID = "/map"
+TAG_DETECTION_TOPIC = "/tag_detections"
 RATE = 25
 
-DEBUG = True
+DEBUG = False
+
 
 class TurretControllerService(object):
     def __init__(self):
         rospy.init_node("turret_controller")  # Initialize the ROS node with a name
+
+        # Interacting with the PTU
         # Set the joint_command publisher
         self.joint_commands = JointGroupCommand("turret", [0.0, 0.0])
         self.pub_cmds = rospy.Publisher(
             PTU_COMMAND_TOPIC, JointGroupCommand, queue_size=1
         )
+
+        rospy.wait_for_service(TURRENT_INFO_SERVICE_NAME)
+        self.srv_turret_info = rospy.ServiceProxy(TURRENT_INFO_SERVICE_NAME, RobotInfo)
+        self.turret_info = self.srv_turret_info("group", "turret")
+        self.pan_limits = [
+            self.turret_info.joint_lower_limits[0],
+            self.turret_info.joint_upper_limits[0],
+        ]
+        self.tilt_limits = [
+            self.turret_info.joint_lower_limits[1],
+            self.turret_info.joint_upper_limits[1],
+        ]
+
         # Define your service
         self.service = rospy.Service("aim_enable", AimEnable, self.service_cb)
         self.initialized = False
@@ -33,6 +52,20 @@ class TurretControllerService(object):
         # Set up tf listener
         self._tf_listener = tf.TransformListener()
 
+        # Set up target detection
+        self.detection_pose = None
+        DETECTION_TOPIC_NAME = rospy.get_param("detection_topic_name", "tag_detections")
+        self._apriltag_sub = rospy.Subscriber(
+            DETECTION_TOPIC_NAME, AprilTagDetectionArray, self._apriltag_detection_cb
+        )
+
+    def _apriltag_detection_cb(self, msg):
+        if msg.detections != []:
+            self.detection_pose = True
+        else:
+            self.detection_pose = False
+        return
+
     def service_cb(self, request):
         # Implement your service callback here
         # This function will be called whenever the service is requested
@@ -40,6 +73,7 @@ class TurretControllerService(object):
         self.aim_enable = request.aim_enable
         self.target_frame_id = request.target_frame_id
 
+        # Check if the target is in the frame
         response = AimEnableResponse()
         response.success = self.aim_enable
         response.message = self.target_frame_id
@@ -70,22 +104,24 @@ class TurretControllerService(object):
         return trans, rot
 
     def track_target(self, event=None):
-        (trans_target, rot_target) = self.lookup_transform(destintation_frame_id=self.target_frame_id, 
-                                                           base_frame_id=MAP_FRAME_ID)
         (trans_ptu, rot_ptu) = self.lookup_transform(TILT_TF_FRAME_ID)
+        (trans_target, rot_target) = self.lookup_transform(
+            destintation_frame_id=self.target_frame_id, base_frame_id=MAP_FRAME_ID
+        )
+
         if self.aim_enable and not self.initialized:
             self.go_home()
             self.initialized = True
-            rospy.loginfo("Initialized")
+            rospy.loginfo("Tracking Initialized")
         elif (
             # not rospy.is_shutdown()
             self.aim_enable
+            and self.detection_pose
             and trans_target is not None
             and rot_target is not None
             and trans_ptu is not None
             and rot_ptu is not None
         ):
-
             x_target = trans_target[0]
             y_target = trans_target[1]
             z_target = trans_target[2]
@@ -102,13 +138,26 @@ class TurretControllerService(object):
             # assumes negative angle tilts down
             tilt_error = math.atan(y_target / z_target)
             tilt_cmd = tilt_theta - tilt_error
-            rospy.loginfo("Pan Error: %f Tilt Error: %f", pan_error, tilt_error) if DEBUG else None
+            (
+                rospy.loginfo("Pan Error: %f Tilt Error: %f", pan_error, tilt_error)
+                if DEBUG
+                else None
+            )
             rospy.loginfo("Pan: %f Tilt: %f", pan_error, tilt_cmd) if DEBUG else None
 
             # Publish to PTU
             self.joint_commands.cmd[0] = pan_cmd
             self.joint_commands.cmd[1] = tilt_cmd
+
+            # bound the pan and tilt commands
+            self.joint_commands.cmd[0] = np.clip(
+                self.joint_commands.cmd[0], self.pan_limits[0], self.pan_limits[1]
+            )
+            self.joint_commands.cmd[1] = np.clip(
+                self.joint_commands.cmd[1], self.tilt_limits[0], self.tilt_limits[1]
+            )
             self.pub_cmds.publish(self.joint_commands)
+
         else:
             # Publish to PTU
             self.joint_commands.cmd[0] = 0
